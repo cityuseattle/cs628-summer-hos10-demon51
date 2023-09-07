03a0fefc92b3f595043284521432878152b75926
demon51 <199509082@qq.com>
2023-09-07 14:46:29 -0700

    Submission for HOS10

---
+++ hos09-client/src/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://stunning-acorn-gg5564qx69pc97v4-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
